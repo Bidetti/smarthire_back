eb1646c3da7ac88828a9e1d041024af6e27934f9
Rafael Bidetti <rafael.bidetti@gmail.com>
2023-11-28 21:58:00 -0300

    Jest & Tests - Setup

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,15 @@
-import express from 'express';
+import app from './app';
 import mongoose from 'mongoose';
-import cors from 'cors';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-import router from './routes/routes';
 import logger from './config/logger';
+import dotenv from 'dotenv';
 
 dotenv.config();
-
-const app = express();
 const port = process.env.PORT || 3000;
-
-
-app.use(cors());
-app.use(helmet());
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-
-app.use('/api', router);
 
 console.log("========================================================================\n");
 console.log(`Iniciando servidor...`);
-mongoose
-  .connect(process.env.MONGODB_URI!)
+
+mongoose.connect(process.env.MONGODB_URI!)
   .then(() => {
     console.log("Configurando ambiente...");
     app.listen(port, () => {
