d9d0c27fdbc94fd06ddbdd36611a668dd1beb84c
Rafael Bidetti <rafael.bidetti@gmail.com>
2023-11-29 00:05:30 -0300

    Test - JWT

--- __tests__/controller/authController.test.ts
+++ __tests__/controller/authController.test.ts
@@ -1,8 +1,10 @@
 import request from "supertest";
 import app from "../../src/app";
 import UserModel from "../../src/models/userModel";
+import RecoverCodeModel from "../../src/models/recoverModel";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { sendEmail } from "../../src/email/config";
 
 let server: any;
 
@@ -15,8 +17,12 @@ afterAll((done) => {
 });
 
 jest.mock("../../src/models/userModel");
+jest.mock("../../src/models/recoverModel");
 jest.mock("bcrypt");
 jest.mock("jsonwebtoken");
+jest.mock("../../src/email/config", () => ({
+  sendEmail: jest.fn(),
+}));
 
 describe("AuthController - conectarUsuario", () => {
   it("200 - User com credenciais corretas", async () => {
@@ -55,7 +61,7 @@ describe("AuthController - conectarUsuario", () => {
     (bcrypt.compare as jest.Mock).mockResolvedValue(false);
 
     const response = await request(app).post("/api/auth").send({
-      email: "",
+      email: "test@gg.com",
       senha: "password",
     });
 
@@ -66,12 +72,126 @@ describe("AuthController - conectarUsuario", () => {
   it("500 - Erro ao conectar o usuário", async () => {
     (UserModel.findOne as jest.Mock).mockRejectedValue(new Error());
 
-    const response = await request(app)
-      .post("/api/auth")
-      .send({ 
-        email: "",
-        senha: "password" 
+    const response = await request(app).post("/api/auth").send({
+      email: "",
+      senha: "password",
     });
+
+    expect(response.statusCode).toBe(500);
+    expect(response.body).toHaveProperty("error");
+  });
+});
+
+// describe("AuthController - recoverCode", () => {
+//   it("200 - Código enviado com sucesso", async () => {
+//     (UserModel.findOne as jest.Mock).mockResolvedValue({
+//       _id: "123",
+//       nomeCompleto: "Test",
+//       email: "test@gm.com",
+//     });
+//     (bcrypt.hash as jest.Mock).mockResolvedValue("fake_hash");
+//     (RecoverCodeModel.prototype.save as jest.Mock).mockResolvedValue(null);
+//     (sendEmail as jest.Mock).mockResolvedValue(null);
+
+//     const response = await request(app)
+//       .post("/api/auth/recover")
+//       .send({ email: "test@gm.com" });
+
+//     expect(response.statusCode).toBe(200);
+//     expect(response.body).toHaveProperty("userID");
+//   });
+
+//   it("404 - Usuário não encontrado", async () => {
+//     (UserModel.findOne as jest.Mock).mockResolvedValue(null);
+
+//     const response = await request(app)
+//       .post("/api/auth/recover")
+//       .send({ email: "test@gg.com" });
+
+//     expect(response.statusCode).toBe(404);
+//     expect(response.body).toHaveProperty("error");
+//   });
+
+//   it("500 - Erro ao enviar o email", async () => {
+//     (UserModel.findOne as jest.Mock).mockResolvedValue({
+//       _id: "123",
+//       nomeCompleto: "Test",
+//     });
+//     (bcrypt.hash as jest.Mock).mockResolvedValue("fake_hash");
+//     (RecoverCodeModel.prototype.save as jest.Mock).mockRejectedValue(new Error());
+//     (sendEmail as jest.Mock).mockResolvedValue(null);
+
+//     const response = await request(app)
+//       .post("/api/auth/recover")
+//       .send({ email: "test@gg.com" });
+
+//     expect(response.statusCode).toBe(500);
+//     expect(response.body).toHaveProperty("error");
+//   });
+
+//   it("500 - Erro ao enviar código de recuperação", async () => {
+//     (UserModel.findOne as jest.Mock).mockImplementation(() => {
+//       return {
+//         exec: () => Promise.reject(new Error())
+//       };
+//     });
+
+//     const response = await request(app)
+//       .post("/api/auth/recover")
+//       .send({ email: "test@gg.com" });
+
+//     expect(response.statusCode).toBe(500);
+//     expect(response.body).toHaveProperty("error");
+//   });
+// });
+
+describe("AuthController - verifyJWT", () => {
+  it("200 - Token válido", async () => {
+    (jwt.verify as jest.Mock).mockImplementation((token, secret, callback) => {
+      callback(null, { userID: "123" });
+    });
+
+    const response = await request(app)
+      .post("/api/auth/verifyjwt")
+      .set("Authorization", "Bearer fake_token");
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty("jwt", true);
+  });
+
+  it("401 - Token não encontrado", async () => {
+    (jwt.verify as jest.Mock).mockImplementation((token, secret, callback) => {
+      callback(new Error());
+    });
+
+    const response = await request(app)
+      .post("/api/auth/verifyjwt")
+
+    expect(response.statusCode).toBe(401);
+    expect(response.body).toHaveProperty("error", "Token não encontrado");
+  });
+
+  it("401 - Token inválido", async () => {
+    (jwt.verify as jest.Mock).mockImplementation((token, secret, callback) => {
+      callback(new Error("Token inválido"), null);
+  });
+
+    const response = await request(app)
+      .post("/api/auth/verifyjwt")
+      .set("Authorization", "Bearer f1132");
+
+    expect(response.statusCode).toBe(401);
+    expect(response.body).toHaveProperty("error", "Token inválido!");
+  });
+
+  it("500 - Erro ao verificar o token", async () => {
+    (jwt.verify as jest.Mock).mockImplementation(() => {
+      throw new Error();
+    });
+
+    const response = await request(app)
+      .post("/api/auth/verifyjwt")
+      .set("Authorization", "Bearer faksa");
 
     expect(response.statusCode).toBe(500);
     expect(response.body).toHaveProperty("error");
